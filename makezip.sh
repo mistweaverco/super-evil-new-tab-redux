316487a789b7937f21afacd2355739d7941f102c
Marco Kellershoff <marco@kellershoff.net>
2021-04-08 10:57:22 +0200

    Make makezip quiet

--- makezip.sh
+++ makezip.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 VERSION=$(jq -r .version < package.json)
-echo "$VERSION"
-zip -r "addon-$VERSION.zip" \
+FILENAME="addon-$VERSION.zip"
+echo "Zipping $FILENAME"
+zip -q -r "$FILENAME" \
 	ace-editor \
 	icons \
 	manifest.json \
